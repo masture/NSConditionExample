c0f92050154f1f7aa8fa993066edd96f55c717dd
Pankaj Kulkarni <kulkarnipanku@gmail.com>
2020-05-16 23:08:29 +0530

    The crash with removing semaphore item from array is fixed by replacing array by dictionary with uuid as key and semaphore as  value. This is mostly working fine... though couple of crashes could not  be reproduced while removing element from the dictionary.

--- NSConditionExample/ViewController.swift
+++ NSConditionExample/ViewController.swift
@@ -25,12 +25,17 @@ class LMSemaphore: Equatable {
         uuid = semaphoreCount
     }
     
+    deinit {
+        print("DEINIT - \(uuid)")
+    }
+    
     
 }
 
 class ViewController: UIViewController {
     
-    var semaphores = [LMSemaphore]()
+    var semaphores = [Int: LMSemaphore]()
+    var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,7 +53,8 @@ class ViewController: UIViewController {
         
         let semaphore = LMSemaphore()
         semaphore.nsCondition.lock()
-        self.semaphores.append(semaphore)
+//        self.semaphores.append(semaphore)
+        semaphores[semaphore.uuid] = semaphore
         print("STARTING METHOD_1 - \(semaphore.uuid) : Semaphore count: \(semaphores.count)")
 //        print("WILL LOCK METHOD 1")
 //        myCondition.lock()
@@ -65,14 +71,17 @@ class ViewController: UIViewController {
             print("DID WAIT METHOD_1 - \(semaphore.uuid)")
         }
 
-        if let index = self.semaphores.firstIndex(where: { (item) -> Bool in
-            return item == semaphore
-        }) {
-            print("METHOD_1: DELETING Semaphore: \(semaphore.uuid)")
-            self.semaphores.remove(at: index)
-        } else {
-            print("METHOD_1: NOT FOUD TO DELETE Semaphore: \(semaphore.uuid)")
-        }
+//        if let index = self.semaphores.firstIndex(where: { (item) -> Bool in
+//            return item == semaphore
+//        }) {
+//            print("METHOD_1: DELETING Semaphore: \(semaphore.uuid)")
+//            self.semaphores.remove(at: index)
+//        } else {
+//            print("METHOD_1: NOT FOUD TO DELETE Semaphore: \(semaphore.uuid)")
+//        }
+//        semaphores[semaphore.uuid] = nil
+        semaphores.removeValue(forKey: semaphore.uuid)
+        
         
         print("WILL UNLOCK METHOD_1 - \(semaphore.uuid)")
         semaphore.nsCondition.unlock()
@@ -83,11 +92,16 @@ class ViewController: UIViewController {
 
     func method2(uuid: Int) {
         
-        guard let semaphore = semaphores.first(where: { (item) -> Bool in
-            return item.uuid == uuid
-        }) else {
+//        guard let semaphore = semaphores.first(where: { (item) -> Bool in
+//            return item.uuid == uuid
+//        }) else {
+//            return
+//        }
+        
+        guard let semaphore = semaphores[uuid] else {
             return
         }
+        
 //        semaphore.nsCondition.lock()
         print("STARTING METHOD_2 - \(semaphore.uuid)")
         let delay = Double.random(in: 5...15)
@@ -111,19 +125,28 @@ class ViewController: UIViewController {
         
         var count = 0
         
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
             count += 1
             DispatchQueue.global(qos: .background).async {
                 self.method1()
             }
 
-            if count >= 20 {
-                print("TIMER STOPED")
-                timer.invalidate()
-            }
+//            if count >= 20 {
+//                print("TIMER STOPED")
+//                timer.invalidate()
+//            }
         }
     }
     
+    @IBAction func checkCountPressed(_ sender: UIButton) {
+        print("Semaphore count: \(semaphores.count)")
+    }
+    
+    @IBAction func stopTimer(_ sender: UIButton) {
+        
+        timer?.invalidate()
+        print("Timer Stopped!!!")
+    }
     
 }
 
